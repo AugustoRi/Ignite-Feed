b2aa4aabee4364de202632808bc5441e97f6f1b7
Augusto Ribeiro <franciscoaugustodev@gmail.com>
2022-11-06 14:37:42 -0300

    feat componente aside com estilizacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+import { Post } from "./components/Post";
+
+import styles from "./App.module.css";
 
 export const App = () => {
   return (
-    <>
+    <div>
       <Header />
-      <h1>Hello World!</h1>
-    </>
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="John Doe"
+            content="lorem ipsum dolor sit amet consectetur adipisicing elit."
+          />
+          <Post
+            author="Homer Simpson"
+            content="lorem ipsum dolor sit amet consectetur adipisicing elit."
+          />
+        </main>
+      </div>
+    </div>
   )
 };
